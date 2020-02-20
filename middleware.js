0250b6f8cf528c94e539ad8e8a66066e53a82541
Gaetano Piazzolla <gae.piaz@gmail.com>
2020-02-20 15:56:26 +0100

    Update middleware.js
    1) moved remove of bearer string inside if(token) otherwise it will call startsWith on undefined value
    2) added login inside middleware.js

--- middleware.js
+++ middleware.js
@@ -1,16 +1,17 @@
+const jwtSecret = 'secret';
 let jwt = require('jsonwebtoken');
-const config = require('./config.js');
 
-let checkToken = (req, res, next) => {
+const checkToken = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
 
   if (token) {
+
     if (token.startsWith('Bearer ')) {
       // Remove Bearer from string
       token = token.slice(7, token.length);
     }
 
-    jwt.verify(token, config.secret, (err, decoded) => {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
       if (err) {
         return res.json({
           success: false,
@@ -29,6 +30,43 @@ let checkToken = (req, res, next) => {
   }
 };
 
+const login = (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  // For the given username fetch user from DB
+  let mockedUsername = 'admin';
+  let mockedPassword = 'password';
+
+  if (username && password) {
+    if (username === mockedUsername && password === mockedPassword) {
+      let token = jwt.sign({username: username},
+        jwtSecret,
+        {
+          expiresIn: '24h' // expires in 24 hours
+        }
+      );
+      // return the JWT token for the future API calls
+      res.json({
+        success: true,
+        message: 'Authentication successful!',
+        token: token
+      });
+    } else {
+      res.send(403).json({
+        success: false,
+        message: 'Incorrect username or password'
+      });
+    }
+  } else {
+    res.send(400).json({
+      success: false,
+      message: 'Authentication failed! Please check the request'
+    });
+  }
+};
+
 module.exports = {
-  checkToken: checkToken
+  checkToken: checkToken,
+  loginJwt: login,
 };
